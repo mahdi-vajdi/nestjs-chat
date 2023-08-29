895d3addf24d5133e758a075969a36f13b40875f
mahdi-vajdi <mahdivajdii@gmail.com>
2023-08-29 22:21:11 +0330

    Created a basic chat api without database or authentication

--- chat-app-backend/src/app.module.ts
+++ chat-app-backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatGateway } from './chat.gateway';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [],
+  imports: [MessagesModule],
   controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ chat-app-backend/src/messages/messages.gateway.ts
@@ -0,0 +1,55 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class MessagesGateway {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @SubscribeMessage('createMessage')
+  create(
+    @MessageBody() createMessageDto: CreateMessageDto,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const message = this.messagesService.create(createMessageDto, client.id);
+    this.server.emit('message', message);
+    return message;
+  }
+
+  @SubscribeMessage('findAllMessages')
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @SubscribeMessage('join')
+  joinRoom(
+    @MessageBody('name') name: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    return this.messagesService.identify(name, client.id);
+  }
+
+  @SubscribeMessage('typing')
+  typing(
+    @MessageBody('isTyping') isTyping: boolean,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const name = this.messagesService.getClientName(client.id);
+
+    client.broadcast.emit('typing', { name, isTyping });
+  }
+}
