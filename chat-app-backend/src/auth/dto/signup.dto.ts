669fbca8db8bf2724d52a45a7402ba7f09fac5c9
mahdi-vajdi <mahdivajdii@gmail.com>
2023-08-31 16:07:17 +0330

    Added validation for dtos

--- chat-app-backend/src/auth/dto/signup.dto.ts
+++ chat-app-backend/src/auth/dto/signup.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString, IsStrongPassword } from 'class-validator';
+
 export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsStrongPassword()
   password: string;
 }
--- chat-app-backend/src/messages/dto/create-message.dto.ts
+++ chat-app-backend/src/messages/dto/create-message.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateMessageDto {
+  @IsString()
+  @IsNotEmpty()
   text: string;
+
+  @IsString()
+  @IsNotEmpty()
   receiver: string;
 }
