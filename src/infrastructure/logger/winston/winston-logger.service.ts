bc2e3402274a064788d2c8e909fe2edabf53d35f
mahdi-vajdi <mahdivajdii@gmail.com>
2024-10-07 22:12:18 +0330

    Add database module to the infrastructure layer

--- src/infrastructure/logger/winston/winston-logger.service.ts
+++ src/infrastructure/logger/winston/winston-logger.service.ts
@@ -1,8 +1,9 @@
 import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { Logger as TypeOrmLogger, QueryRunner } from 'typeorm';
 
 @Injectable()
-export class WinstonLoggerService implements LoggerService {
+export class WinstonLoggerService implements LoggerService, TypeOrmLogger {
   private epoch = 0;
   private preTimestamp = 0;
 
@@ -11,18 +12,9 @@ export class WinstonLoggerService implements LoggerService {
     private readonly logger: LoggerService,
   ) {}
 
-  private getEpoch(): string {
-    const now = Date.now();
-    if (now === this.preTimestamp) {
-      this.epoch++;
-    } else {
-      this.epoch = 0;
-    }
-    this.preTimestamp = now;
-
-    return `${this.preTimestamp}${this.epoch.toString().padStart(3, '0')}`;
-  }
-
+  /*
+  Implementing methods for nestjs logger service
+   */
   debug?(message: any, ...optionalParams: any[]) {
     this.logger.debug(
       {
@@ -82,5 +74,85 @@ export class WinstonLoggerService implements LoggerService {
       },
       optionalParams,
     );
+  }
+
+  /*
+  Implementing methods for typeorm logger service
+   */
+  logQuery(query: string, parameters?: any[], queryRunner?: QueryRunner) {
+    this.logger.debug(
+      {
+        message: `[Query]: ${query}; [Parameters]: [${[parameters]}]`,
+        epoch: this.getEpoch(),
+      },
+      ['Database'],
+    );
+  }
+
+  logQueryError(
+    error: string | Error,
+    query: string,
+    parameters?: any[],
+    queryRunner?: QueryRunner,
+  ) {
+    this.logger.error(
+      {
+        message: `[Error]: error ${error}. [Query]: ${query}; [Parameters]: [${[
+          parameters,
+        ]}]`,
+        epoch: this.getEpoch(),
+      },
+      ['Database'],
+    );
+  }
+
+  logQuerySlow(
+    time: number,
+    query: string,
+    parameters?: any[],
+    queryRunner?: QueryRunner,
+  ) {
+    this.logger.warn(
+      {
+        message: `[Slow Query (${time} ms)]: ${query}; [Parameters]: [${[
+          parameters,
+        ]}]`,
+        epoch: this.getEpoch(),
+      },
+      ['Database'],
+    );
+  }
+
+  logSchemaBuild(message: string, queryRunner?: QueryRunner) {
+    this.logger.log(
+      {
+        message: `[Schema Build]: ${message}`,
+        epoch: this.getEpoch(),
+      },
+      ['Database'],
+    );
+  }
+
+  logMigration(message: string, queryRunner?: QueryRunner) {
+    this.logger.log(
+      {
+        message: `[Migration]: ${message}`,
+        epoch: this.getEpoch(),
+      },
+      ['Database'],
+    );
+  }
+
+  // This method is for getting a unified timestamp for logs
+  private getEpoch(): string {
+    const now = Date.now();
+    if (now === this.preTimestamp) {
+      this.epoch++;
+    } else {
+      this.epoch = 0;
+    }
+    this.preTimestamp = now;
+
+    return `${this.preTimestamp}${this.epoch.toString().padStart(3, '0')}`;
   }
 }
