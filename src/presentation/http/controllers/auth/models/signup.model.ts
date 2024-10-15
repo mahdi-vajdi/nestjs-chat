9ea41790332ea4996f525f93ad41344b5328e7a9
mahdi-vajdi <mahdivajdii@gmail.com>
2024-10-16 00:55:38 +0330

    Add class-validator and class-transformer to the project

--- src/presentation/http/controllers/auth/models/signup.model.ts
+++ src/presentation/http/controllers/auth/models/signup.model.ts
@@ -1,16 +1,21 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class SignupRequest {
   @ApiProperty()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   password: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
   firstName: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
   lastName: string;
 }
 
