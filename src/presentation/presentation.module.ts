9a6c32d95bdb3c76f61601ba13e9df4d783a9823
mahdi.vajdi <mahdivajdii@gmail.com>
2024-10-31 13:25:29 +0330

    Feature: Create skeleton gateway

--- src/presentation/presentation.module.ts
+++ src/presentation/presentation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../application/user/user.module';
 import { AuthHttpController } from './http/controllers/auth/auth-http.controller';
+import { ConversationGateway } from './websocket/gateways/conversation.gateway';
 
 @Module({
   imports: [UserModule],
   controllers: [AuthHttpController],
+  providers: [ConversationGateway],
 })
 export class PresentationModule {}
